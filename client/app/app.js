a33b147f7dbb9848c31f25ebe6614ccba920ae4a
Blu2z <blu2z@yandex.ru>
2016-05-13 18:46:27 +0300

    ribbon panel

--- client/app/app.js
+++ client/app/app.js
@@ -26,10 +26,7 @@ angular.module('meanApp', [
   .config(function($urlRouterProvider, $locationProvider, $stateProvider) {  
 
     $urlRouterProvider.deferIntercept();
-
     $urlRouterProviderRef = $urlRouterProvider;
-    
-    // $locationProvider.html5Mode(false);
     $stateProviderRef = $stateProvider;
 
     $urlRouterProvider
@@ -49,27 +46,12 @@ angular.module('meanApp', [
     }); 
   });
 
-// class getStates {
-
-//   constructor(q, $rootScope, $state, $http, AdminService) {
-//     $http.get('/api/things').then(response => {})
-//   }
-// }
-
-// angular.module('meanApp')
-//   .run('getStates', getStates);
-
 angular.module('meanApp')
-  .run(['$q', '$rootScope', '$state', '$http', '$urlRouter', '_',
-  function ($q, $rootScope, $state, $http, $urlRouter, _) {
+  .run(['$http', '$urlRouter',
+  function ( $http, $urlRouter) {
 
     function constructTpl(templateUrl, layout) {
-      if(!layout) {
-        return '<div class="test" ng-include="\'' + templateUrl + '\'"></div>';
-      }
-
-      // var result = '';
-      // console.debug(layout);
+      if(!layout) {return '<div class="test" ng-include="\'' + templateUrl + '\'"></div>';}
 
       var parseLayout = function (data) {
         var txt = '';
@@ -77,58 +59,44 @@ angular.module('meanApp')
 
         angular.forEach(data, function (value, key) {
 
-          cls = value.class ? 'class="' + value.class + '"' : '';
+          cls = value.class ? `class="${value.class}"` : '';
           child = value.children ? parseLayout(value.children) : '';
-
-          if (data[1]) {
-            txt += `<${value.tag} ${cls}>${child}</${value.tag}>`
-            // console.debug('+=');
-            // result += txt;
-          } else {
-            txt = `<${value.tag} ${cls}>${child}</${value.tag}>`
-            
-          }
-          // console.debug('data: ', data);
-          // console.debug('txt: ', txt);
+          txt = (data[1]) 
+            ? txt + `<${value.tag} ${cls}>${child}</${value.tag}>`
+            : `<${value.tag} ${cls}>${child}</${value.tag}>`;
         });
-        
         return txt
       };
-
-      // console.debug('result: ', result);
       return parseLayout(layout);
     };
 
-    function constructCtrl (layout) {
-       
-    }
+    function constructCtrl (layout) {}
 
-    // ng-include="'app/view.html'"
     $http.get('api/app')
-    .success(function(data) {
-      angular.forEach(data[0].pages, function (value, key) { 
-          var state = {
-            "url": value.url,
-            "parent" : value.parent,
-            "abstract": value.abstract,
-            "template": constructTpl(value.templateUrl, value.layout),
-            // "template": constructTpl.call(value.templateUrl),
-            // "templateUrl": constructTpl.call(value.templateUrl),
-            "controller": value.controller,
-            "controllerAs": value.controllerAs
-          };
-          
-          // angular.forEach(value.views, function (view) {
-          //   state.views[view.name] = {
-          //     templateUrl : view.templateUrl,
-          //   };
-          // });
+      .success(function(data) {
+        angular.forEach(data[0].pages, function (value, key) { 
+            var state = {
+              "url": value.url,
+              "parent" : value.parent,
+              "abstract": value.abstract,
+              "template": constructTpl(value.templateUrl, value.layout),
+              // "template": constructTpl.call(value.templateUrl),
+              // "templateUrl": constructTpl.call(value.templateUrl),
+              "controller": value.controller,
+              "controllerAs": value.controllerAs
+            };
+            
+            // angular.forEach(value.views, function (view) {
+            //   state.views[view.name] = {
+            //     templateUrl : view.templateUrl,
+            //   };
+            // });
 
-          $stateProviderRef.state(value.name, state);
+            $stateProviderRef.state(value.name, state);
+        });
+
+        $urlRouter.sync();
+        $urlRouter.listen();
+
       });
-
-      $urlRouter.sync();
-      $urlRouter.listen();
-
-    });
 }]);
--- client/components/admin/constructor/constructorAdmin.controller.js
+++ client/components/admin/constructor/constructorAdmin.controller.js
@@ -7,9 +7,19 @@ class ConstructorAdminController {
 
     var pages = this;
 
-    pages.name = "one";
+    pages.grids = [
+    	{name: 'one', url: 'components/grids/one.html', img: 'assets/images/grid-1.jpg'},
+    	{name: 'two', url: 'components/grids/two.html', img: 'assets/images/grid-2.jpg'},
+    	{name: 'three', url: 'components/grids/three.html', img: 'assets/images/grid-3.jpg'},
+    	{name: 'four', url: 'components/grids/four.html', img: 'assets/images/grid-4.jpg'}
+    ];
+
+    pages.grid = 0;
+    // pages.currentGrid = `components/grids/${pages.name}.html`
   }
-    
+
+
+
 }
 
 angular.module('meanApp')
